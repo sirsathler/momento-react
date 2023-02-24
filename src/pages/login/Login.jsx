ac5e4a3e39eb05a6316f9fcb5bf9c995811270ae
Matheus Sathler <mattsathler@gmail.com>
2023-02-24 11:47:01 -0300

    add login with momento button layout

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,6 @@
 import './Login.scss';
 import logo from '../../assets/logo/new-big.png'
+import logo_alt from '../../assets/logo/new-transparent.png'
 import discord_logo from '../../assets/others/discord.png'
 
 export default function () {
@@ -9,11 +10,17 @@ export default function () {
                 <img src={logo} alt="momento-logo" />
             </div>
             <div className="generic-container">
-                <a className="login-button">
-                    <div className="login-button__dc-logo-container">
+                <a className="login-button discord">
+                    <div className="login-button__login-logo-container">
                         <img src={discord_logo} alt="next" />
                     </div>
-                    <p className="login-button__label">Login with Discord</p>
+                    <p className="login-button__label discord">Continuar com Discord</p>
+                </a>
+                <a className="login-button momento disabled">
+                    <div className="login-button__login-logo-container">
+                        <img src={logo_alt} alt="next" />
+                    </div>
+                    <p className="login-button__label">Continuar com momento</p>
                 </a>
             </div>
 
