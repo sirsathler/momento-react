ce3dc9fc67ba60b8b1ed65e14f5b5dbb1bf79445
Matheus Sathler <mattsathler@gmail.com>
2023-02-27 21:54:56 -0300

    Create post layout

--- src/pages/feed/Feed.jsx
+++ src/pages/feed/Feed.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthProvider";
 import { Navbar } from "../../global/components/navbar/Navbar.jsx";
 import UserSelect from "../user-select/UserSelect";
+import { Post } from "./Post/Post";
 
 import './Feed.scss'
 
@@ -11,6 +12,12 @@ export function Feed() {
         return (
             <div className="feed-page">
                 <Navbar />
+                <div className="feed-container">
+                    <Post postImageURL={'https://cdn.discordapp.com/attachments/1073706433750761562/1073997069322293309/file.jpg'}/>
+                    <Post postImageURL={'https://static.vecteezy.com/ti/fotos-gratis/p3/6671766-fantastica-lua-magica-luz-e-agua-barco-com-arvore-papel-de-parede-gratis-foto.jpg'}/>
+                    <Post postImageURL={'https://cdn.discordapp.com/attachments/1073706433750761562/1073997069322293309/file.jpg'}/>
+                    <Post postImageURL={'https://cdn.discordapp.com/attachments/1073706433750761562/1073997069322293309/file.jpg'}/> 
+                </div>
             </div>
         )
     }
---
+++ src/pages/feed/Post/Post.jsx
@@ -0,0 +1,23 @@
+import './Post.scss'
+
+export function Post(props) {
+    return (
+        <div className="post-container">
+            <div className="post-header">
+                <div className="post-header__profile-picture">
+                    <img src="https://cdn.discordapp.com/attachments/1073706433750761562/1073997069322293309/file.jpg" alt="profile-picture" />
+                </div>
+                <div className="post-user-info">
+                    <p className="post-user-info__user-name">Doug Castelano</p>
+                    <p className="post-user-info__username light-font">@dcastelano</p>
+                </div>
+            </div>
+            <div className="post-body">
+                <img src={props.postImageURL} alt="post-image" />
+            </div>
+            <div className="post-footer">
+
+            </div>
+        </div>
+    )
+}
